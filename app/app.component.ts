71d877b0b48362fde3bb2a929831c4518fec0e0c
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-04-26 16:26:51 +0300

    Add client part

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['../style.css']
+})
+
+export class AppComponent{
+    title:string = "App Works";
+}
