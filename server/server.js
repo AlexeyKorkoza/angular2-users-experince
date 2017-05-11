85beed1c016887f87abc6e9436da830517175a7f
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-05-11 17:17:03 +0300

    Remove ejs

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var ejs = require('ejs');
 var mongoose = require("mongoose");
 var morgan = require("morgan");
 var app = express();
@@ -17,10 +16,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('html', ejs.renderFile);
 
 app.use(morgan("dev"));
 
