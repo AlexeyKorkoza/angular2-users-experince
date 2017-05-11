510b142df626ad0d888a9efdc454265ff5b6df7f
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-05-11 17:15:48 +0300

    Remove comments

--- server/server.js
+++ server/server.js
@@ -24,19 +24,6 @@ app.engine('html', ejs.renderFile);
 
 app.use(morgan("dev"));
 
-/*app.use(function(req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "POST, PUT, OPTIONS, DELETE, GET"
-  );
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});*/
-
 app.use(require("./routers"));
 
 app.listen(port, function () {
