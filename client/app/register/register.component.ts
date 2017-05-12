dccb09d949c5a931b89f201e0dcae5702a38737d
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-05-12 17:34:32 +0300

    Add compare passwords

--- client/app/register/register.component.ts
+++ client/app/register/register.component.ts
@@ -16,6 +16,7 @@ export class RegisterComponent {
     model: any = {};
     registerForm: any;
     message: string = "";
+    password_check: string = "";
 
     constructor(
         private userService: UserService,
@@ -25,12 +26,17 @@ export class RegisterComponent {
                 'username' : [null, Validators.required],
                 'email': [null, Validators.required],
                 'password': [null, Validators.compose([Validators.required, Validators.minLength(6)])],
-                'confirm-password': [null ,Validators.required]
+                'confirm_password': [null ,Validators.required]
             })
          }
 
     register(value: any) {
         this.message = "";
+        this.password_check = "";
+        console.log(value);
+        if(value.password != value.confirm_password){
+            this.password_check = "Пароли не совпадают";
+        } else {
         let date = new Date();
         let current_date = date.getDay() + "." + date.getMonth() + "." + date.getFullYear();
         let time = date.getHours() + "." + date.getMinutes() + "." + date.getSeconds();
@@ -49,5 +55,6 @@ export class RegisterComponent {
                     console.log(error);
                  }
              )
+    }
     }
 }
