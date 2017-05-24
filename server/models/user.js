1a872606c01a097aa4794ff0ae46685cca31d00e
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-05-24 21:59:44 +0300

    Add generate jwt for user

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,7 @@
 var mongoose = require("mongoose");
 var bcrypt = require("bcrypt-nodejs");
+var jwt = require("jsonwebtoken");
+var jwtSecret = "Anaheim Mighty Ducks";
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
@@ -23,6 +25,16 @@ userSchema.methods.generateHash = function(password) {
 userSchema.methods.validPassword = function(user, password) {
      return bcrypt.compareSync(password, user.password);
 };
+
+userSchema.methods.generateJWT = function(){
+
+  return jwt.sign({
+    id: this._id,
+    username: this.username,
+    exp: Math.floor(Date.now() / 1000) + (60 * 60)
+  }, jwtSecret);
+
+}
 
 var User = mongoose.model('User', userSchema);
 
--- server/passport/passport.js
+++ server/passport/passport.js
@@ -1,7 +1,5 @@
 var LocalStrategy = require("passport-local").Strategy;
-var jwt = require('jsonwebtoken');
 var User = require('../models/user');
-var jwtSecret = "Anaheim Mighty Ducks";
 
 module.exports = function(passport) {
   passport.use("local-login", new LocalStrategy({
@@ -18,17 +16,11 @@ module.exports = function(passport) {
           }
           if (!user.validPassword(user, password)) {
             return done(null, false, req.flash("loginMessage", "Oops! Wrong password."));
-          }         
-
-          var payload = {
-            sub: user._id
           }
-
-          var token = jwt.sign(payload, jwtSecret);
 
           var userData = {
            "username": user.username,
-           "token": token
+           "token": user.generateJWT()
           }
           
           return done(null, userData);
