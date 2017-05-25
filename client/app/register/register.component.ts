650caa52bc9541c600479fd6d6d7fa473e54d0dd
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-05-25 11:52:12 +0300

    Remove console.log and comments

--- client/app/register/register.component.ts
+++ client/app/register/register.component.ts
@@ -1,8 +1,7 @@
-import {Component} from "@angular/core";
-import {Router} from "@angular/router";
-import {UserService} from "../services/user.service";
-import {FormGroup, FormBuilder, Validators} from "@angular/forms";
-import {error} from "util";
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { UserService } from "../services/user.service";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
     selector: "register",
@@ -21,41 +20,30 @@ export class RegisterComponent {
         private userService: UserService,
         private router: Router,
         private formBuilder: FormBuilder) {
-            this.registerForm = formBuilder.group({
-                'username' : [null, Validators.required],
-                'email': [null, Validators.required],
-                'password': [null, Validators.compose([Validators.required, Validators.minLength(6)])],
-                'confirm_password': [null ,Validators.required]
-            })
-         }
+        this.registerForm = formBuilder.group({
+            'username': [null, Validators.required],
+            'email': [null, Validators.required],
+            'password': [null, Validators.compose([Validators.required, Validators.minLength(6)])],
+            'confirm_password': [null, Validators.required]
+        })
+    }
 
     register(value: any) {
         this.message = "";
         this.password_check = "";
-        console.log(value);
-        if(value.password != value.confirm_password){
+        if (value.password != value.confirm_password) {
             this.password_check = "Пароли не совпадают";
         } else {
-        let date = new Date();
-        let current_date = date.getDay() + "." + date.getMonth() + "." + date.getFullYear();
-        let time = date.getHours() + "." + date.getMinutes() + "." + date.getSeconds();
-        value.time = time;
-        value.date = current_date;
-        this.userService.create(value)
-             .subscribe(
-                 data => {
-                    //  if(data.success){
-                    //      
-                    //  } else {
-                    //      this.message = data.message;
-                    //  }
-                    console.log(data);
+            let date = new Date();
+            let current_date = date.getDay() + "." + date.getMonth() + "." + date.getFullYear();
+            let time = date.getHours() + "." + date.getMinutes() + "." + date.getSeconds();
+            value.time = time;
+            value.date = current_date;
+            this.userService.create(value).subscribe(
+                data => {
                     this.router.navigate(['/']);
-                 },
-                 error => {
-                    console.log(error);
-                 }
-             )
-    }
+                }
+                )
+        }
     }
 }
