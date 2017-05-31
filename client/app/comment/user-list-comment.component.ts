3c746513e931b87e91f019a89e08b8ee6d1d8856
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-05-31 18:49:39 +0300

    Remove dropdown, create components for the profile

---
+++ client/app/comment/user-list-comment.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'user-list-comments',
+    templateUrl: 'user-list-comment.component.html'
+})
+
+export class UserListCommentComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
