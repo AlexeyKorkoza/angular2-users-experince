842cf3bb61630b14bb20b11811e7036cd02dbca0
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-06-14 21:02:36 +0300

    #24 - Add angular2-loading-bar

--- client/app/login/login.component.ts
+++ client/app/login/login.component.ts
@@ -1,8 +1,9 @@
-import { OnInit, Component } from "@angular/core";
-import { FormBuilder, Validators} from "@angular/forms";
-import { Router, ActivatedRoute } from "@angular/router";
+import {OnInit, Component} from "@angular/core";
+import {FormBuilder, Validators} from "@angular/forms";
+import {Router, ActivatedRoute} from "@angular/router";
+import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
 
-import { AuthenticationService } from "../services/authentication.service"; 
+import {AuthenticationService} from "../services/authentication.service";
 
 @Component({
     moduleId: module.id,
@@ -10,29 +11,32 @@ import { AuthenticationService } from "../services/authentication.service";
     templateUrl: "login.component.html"
 })
 
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
 
     loginForm: any;
     returnUrl: string;
     message: string = "";
 
-    constructor(private authenticationService: AuthenticationService, 
+    constructor(private authenticationService: AuthenticationService,
                 private formBuilder: FormBuilder,
                 private router: Router,
-                private route: ActivatedRoute){
+                private route: ActivatedRoute,
+                private slimLoadingBarService: SlimLoadingBarService) {
         this.loginForm = formBuilder.group({
-            "username": [null, Validators.required], 
+            "username": [null, Validators.required],
             "password": [null, Validators.required]
         });
     }
 
-    ngOnInit(){
-         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    ngOnInit() {
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
 
-    login(value: any){
+    login(value: any) {
+        this.slimLoadingBarService.start();
         this.authenticationService.login(value).subscribe(
-            data => {
+            () => {
+                this.slimLoadingBarService.complete();
                 this.router.navigate(['']);
             });
     }
