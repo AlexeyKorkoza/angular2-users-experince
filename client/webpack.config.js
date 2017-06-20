3d594deba3d58136d4ebc53ad2b7df9e3d71a63f
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-06-20 12:27:03 +0300

    #6 - Add jquery to webpack, remove commenting the code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -54,9 +54,11 @@ module.exports = {
       {}
     ),
 
-    /*new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'vendor', 'polyfills']
-    }),*/
+    new webpack.ProvidePlugin({
+      $: "jquery/dist/jquery.min.js",
+      jQuery: "jquery/dist/jquery.min.js",
+      "window.jQuery": "jquery/dist/jquery.min.js"
+    }),
 
     new HtmlWebpackPlugin({
       template: 'src/index.html'
